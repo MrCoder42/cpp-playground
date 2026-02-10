f61807ab3a1b6835e9232ec6f517febf91404d99
Ashish Rai <ashishrai@duck.com>
2026-02-11 00:38:03 +0530

    feat: create program for arrays basics
    – Declare the array
    – Initialise the array feat. aggregate initialisation

---
+++ src/array/array_101.cpp
@@ -0,0 +1,42 @@
+/*
+ * File:    array_101.cpp
+ * Author:  Ashish Rai
+ * Date:    10 February 2026
+ *
+ * Program to demonstrate the basics of the Arrays data structure.
+ */
+
+#include <iomanip>  // setw()
+#include <iostream>
+#include <string>
+
+int main() {
+    // Array Decleration
+    // std::string car[5];
+    // Initialisation via indexing operator
+    // car[0] = "BMW";
+
+    // Aggregate Initialisation
+    std::string car[5] = {"BMW", "Mercedes", "Toyota", "Volvo", "Jaguar"};
+    int car_sales[5];
+
+    // Access the elements
+    car_sales[0] = 5;
+    car_sales[1] = 9;
+    car_sales[2] = 29;
+    car_sales[3] = 17;
+    car_sales[4] = 6;
+
+    std::cout << "––– Getting started with Arrays ---" << std::endl;
+
+    for (int i = 0; i < 5; i++) {
+        std::cout << std::setw(8) << car[i] << " – " << car_sales[i]
+                  << std::endl;
+    }
+
+    std::cout << "\n\nCongratulation! You got started with the Arrays."
+              << std::endl;
+    std::cout << "--------------------" << std::endl;
+
+    return 0;
+}
